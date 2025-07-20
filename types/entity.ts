7707714b8483d9a61fa989c8755da1b7ef452b25
driofrio91 <dannyriofriojimenez@gmail.com>
2025-07-20 23:59:14 +0200

    add entity view model

---
+++ types/entity.ts
@@ -0,0 +1,9 @@
+export type EntityViewModel = {
+    id: string;
+    name: string;
+    description?: string;
+    price?: string;
+    location: string;
+    image?: string;
+    owner?: string;
+};
