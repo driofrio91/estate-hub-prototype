3877390b419ac25a70804a526710998e9bc2d51b
driofrio91 <dannyriofriojimenez@gmail.com>
2025-07-21 17:40:35 +0200

    implement factory in hook and service

--- lib/data/entityService.ts
+++ lib/data/entityService.ts
@@ -2,10 +2,11 @@ import { generateClient } from 'aws-amplify/data';
 import { Schema } from '@/amplify/data/resource';
 import { EntityViewModel } from '@/types/entity';
 import { mapEstateToViewModel } from '@/lib/mappers/entityMapper';
+import { AuthorizationMode } from '@/lib/data/authModes';
 
-const client = generateClient<Schema>();
-
-export function suscribeToEstates(onUpdate: (data: EntityViewModel[]) => void){
+export function suscribeToEstates(onUpdate: (data: EntityViewModel[]) => void, 
+authMode: AuthorizationMode = AuthorizationMode.API_KEY) {
+    const client = generateClient<Schema>({ authMode });
     return client.models.Estate.observeQuery().subscribe({
         next: ({items}) => {
             const estates = items.map(mapEstateToViewModel);
