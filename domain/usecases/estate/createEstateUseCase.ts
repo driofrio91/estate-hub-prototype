a108d99e6fe17a86085dabc00768bb5fbbc562cb
driofrio91 <dannyriofriojimenez@gmail.com>
2025-08-12 18:26:41 +0200

    format

--- domain/usecases/estate/createEstateUseCase.ts
+++ domain/usecases/estate/createEstateUseCase.ts
@@ -6,19 +6,19 @@ import { EstateEntity } from '@/domain/models/estate';
 export async function createEstateUseCase(estate: CreateEstateInput, authMode: AuthorizationMode): Promise<EstateEntity> {
 
   const client = clients[authMode];
-  
+
   try {
     const { data } = await client.models.Estate.create(estate);
     if (!data) {
       throw new Error('Error creating estate, empty data returned');
     }
     return data;
-    
+
   } catch (error) {
     if (error instanceof Error) {
-    throw new Error('Error creating estate: ' + error.message);
-  } else {
-    throw new Error('Error creating estate: ' + String(error));
-  }
+      throw new Error('Error creating estate: ' + error.message);
+    } else {
+      throw new Error('Error creating estate: ' + String(error));
+    }
   }
 }
